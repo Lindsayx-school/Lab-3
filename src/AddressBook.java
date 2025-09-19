082d6943fad0d4bff45a3f471322c55ba6938768
LindsayXu123 <bufferfly137@gmail.com>
2025-09-18 22:07:45 -0400

    updated public static void main to say another change

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -28,6 +28,7 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("Another change");
     }
 }
 
