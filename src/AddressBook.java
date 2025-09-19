ecfb7296d11f30096923efc96a402e31493c63f0
LindsayXu123 <bufferfly137@gmail.com>
2025-09-18 22:22:17 -0400

    commit branch

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -23,6 +23,10 @@ public class AddressBook {
         }
     }
 
+    public void newMethod(){
+        System.out.println("branch");
+    }
+
     public static void main(String[] args){
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
